d77208e70895c674bfd7825e96fe098c959bcc15
Amanda Silva <amanda328@hotmail.com>
2024-04-18 12:23:57 +0100

    build and other package configs

---
+++ dist/routes/get-attendee-badge.mjs
@@ -0,0 +1,8 @@
+import {
+  getAttendeeBadge
+} from "../chunk-XEBVUBJ5.mjs";
+import "../chunk-JRO4E4TH.mjs";
+import "../chunk-JV6GRE7Y.mjs";
+export {
+  getAttendeeBadge
+};
---
+++ dist/server.mjs
@@ -0,0 +1,65 @@
+import {
+  registerForEvent
+} from "./chunk-66ACHKQR.mjs";
+import {
+  errorHandler
+} from "./chunk-E33JLYRM.mjs";
+import {
+  checkIn
+} from "./chunk-CHZFM7MR.mjs";
+import {
+  createEvent
+} from "./chunk-KHCWPFPZ.mjs";
+import "./chunk-677O5SV4.mjs";
+import {
+  getAttendeeBadge
+} from "./chunk-XEBVUBJ5.mjs";
+import {
+  getEventAttendees
+} from "./chunk-ZJ6YT3AV.mjs";
+import {
+  getEvent
+} from "./chunk-3BK2S2WR.mjs";
+import "./chunk-JRO4E4TH.mjs";
+import "./chunk-JV6GRE7Y.mjs";
+
+// src/server.ts
+import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
+var app = fastify();
+app.register(fastifyCors, {
+  origin: "*"
+});
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description: "Especifica\xE7\xF5es da API para o back-end da aplica\xE7\xE3o pass.in constru\xEDda durante o NLW Unite da Rocketseat",
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs"
+});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+app.setErrorHandler(errorHandler);
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
+export {
+  app
+};
